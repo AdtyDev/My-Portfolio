5174b2b499217c3be37361dde088437e446a7fcf
Aditya Gupta <ag012757@gmail.com>
2025-02-10 04:57:44 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -50,7 +50,7 @@ const About = () =>{
                             <div>
                                 <h3 className="text-2xl font-bold text-transparent bg-clip-text 
                                 bg-gradient-to-r from-green-400 to-blue-500">
-                                    3+
+                                    0.5+
                                 </h3>
                                 <p>Experience</p>
                             </div>
